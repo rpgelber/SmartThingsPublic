bf0e70c53741db55ff913a20f40dedf26ecb268a
Aaron Turner <synfinatic@gmail.com>
2018-02-19 20:15:29 -0800

    first real pass at htd-gw-sl1 manager

--- smartapps/synfinatic/htd-gw-sl1-manager.src/htd-gw-sl1-manager.groovy
+++ smartapps/synfinatic/htd-gw-sl1-manager.src/htd-gw-sl1-manager.groovy
@@ -14,11 +14,11 @@
  *
  */
 definition(
-    name: "HTD GW-SL1 Service Manager",
+    name: "HTD (W)GW-SL1 Service Manager",
     namespace: "synfinatic",
     author: "Aaron Turner",
     description: "HTD (W)GW-SL1 Smart Gateway\r\n\r\nAllows controlling your HTD compatible whole house audio system.\r\n\r\nhttps://www.htd.com/GW-SL1\r\nhttps://www.htd.com/WGW-SL1",
-    category: "Fun & Social",
+    category: "Music & Sounds",
     iconUrl: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience.png",
     iconX2Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png",
     iconX3Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png"
@@ -26,34 +26,24 @@ definition(
 
 
 preferences {
-/*
-    page(name: "searchTargetSelection", title: "HTD (W)GW-SL1 Search Target", nextPage: "deviceDiscovery") {
-        section("Search Target") {
-            input "searchTarget", "string", title: "Search Target", defaultValue: "urn:micasaverde-com:serviceId:HTD1", required: false
-        }
-    }
-    page(name: "deviceDiscovery", title: "(W)GW-SL1 Setup", content: "deviceDiscovery")
-*/
     section("IP Address of (W)GW-SL1") {
         input "ipAddress", "string", multiple: false, required: true
     }
     section("TCP Port of (W)GW-SL1") {
-        input "tcpPort", "integer", multiple: false, required: true, defaultValue: 10006
+        input "tcpPort", "integer", multiple: false, required: true, defaultValue: 10006, hideable: true, hidden: true, 
     }
     section("SmartThings Hub") {
-        input "hub", "hub", title: "Select Hub"
+        input "theHub", "string", multiple: false, required: true, options: location.hubs.collect { it.name }
     }
 }
 
 def installed() {
     log.debug "Installed with settings: ${settings}"
-
     initialize()
 }
 
 def updated() {
     log.debug "Updated with settings: ${settings}"
-
     unsubscribe()
     initialize()
 }
@@ -63,112 +53,9 @@ def initialize() {
     def iphex = convertIPtoHex($ipAddress)
     def porthex = convertPortToHex($tcpPort)
     def dni = "${iphex}:${porthex}"
-    def selectedDevice = devices.find { it.value.mac == dni }
-    def d = getChildDevices()?.find {
-        it.deviceNetworkId == selectedDevice.value.mac
-    }
-    if (!d) {
-        log.debug "Adding new HTD (W)GW-SL1 with dni: ${
-    d = addChildDevice("synfinatic", "HTD WG-SL1", dni, theHub, 
-}
-
-void ssdpDiscover() {
-    sendHubCommand(new physicalgraph.device.HubAction("lan discovery ${searchTarget}", physicalgraph.device.Protocol.LAN))
-}
-
-void ssdpSubscribe() {
-    subscribe(location, "ssdpTerm.${searchTarget}", ssdpHandler)
-}
-
-Map verifiedDevices() {
-    def devices = getVerifiedDevices()
-    def map = [:]
-    devices.each {
-        def value = it.value.name ?: "UPnP Device ${it.value.ssdpUSN.split(':')[1][-3..-1]}"
-        def key = it.value.mac
-        map["${key}"] = value
-    }
-    map
-}
-
-void verifyDevices() {
-    def devices = getDevices().findAll { it?.value?.verified != true }
-    devices.each {
-        int port = convertHexToInt(it.value.deviceAddress)
-        String ip = convertHexToIP(it.value.networkAddress)
-        String host = "${ip}:${port}"
-        sendHubCommand(new physicalgraph.device.HubAction("""GET ${it.value.ssdpPath} HTTP/1.1\r\nHOST: $host\r\n\r\n""", physicalgraph.device.Protocol.LAN, host, [callback: deviceDescriptionHandler]))
-    }
-}
-
-def getVerifiedDevices() {
-    getDevices().findAll{ it.value.verified == true }
-}
-
-def getDevices() {
-    if (!state.devices) {
-        state.devices = [:]
-    }
-    state.devices
-}
-
-def addDevices() {
-    def devices = getDevices()
-
-    selectedDevices.each { dni ->
-        def selectedDevice = devices.find { it.value.mac == dni }
-        def d
-        if (selectedDevice) {
-            d = getChildDevices()?.find {
-                it.deviceNetworkId == selectedDevice.value.mac
-            }
-        }
-
-        if (!d) {
-            log.debug "Creating HTD (W)GW-SL1 with dni: ${selectedDevice.value.mac}"
-            addChildDevice("synfinatic", "HTD (W)GW-SL1", selectedDevice.value.mac, selectedDevice?.value.hub, [
-                    "label": selectedDevice?.value?.name ?: "HTD (W)GW-SL1",
-                    "data": [
-                        "mac": selectedDevice.value.mac,
-                        "ip": selectedDevice.value.networkAddress,
-                        "port": selectedDevice.value.deviceAddress
-                    ]
-            ])
-        }
-    }
-}
-
-def ssdpHandler(evt) {
-    def description = evt.description
-    def hub = evt?.hubId
-
-    def parsedEvent = parseLanMessage(description)
-    parsedEvent << ["hub":hub]
-
-    def devices = getDevices()
-    String ssdpUSN = parsedEvent.ssdpUSN.toString()
-    if (devices."${ssdpUSN}") {
-        def d = devices."${ssdpUSN}"
-        if (d.networkAddress != parsedEvent.networkAddress || d.deviceAddress != parsedEvent.deviceAddress) {
-            d.networkAddress = parsedEvent.networkAddress
-            d.deviceAddress = parsedEvent.deviceAddress
-            def child = getChildDevice(parsedEvent.mac)
-            if (child) {
-                child.sync(parsedEvent.networkAddress, parsedEvent.deviceAddress)
-            }
-        }
-    } else {
-        devices << ["${ssdpUSN}": parsedEvent]
-    }
-}
-
-void deviceDescriptionHandler(physicalgraph.device.HubResponse hubResponse) {
-    def body = hubResponse.xml
-    def devices = getDevices()
-    def device = devices.find { it?.key?.contains(body?.device?.UDN?.text()) }
-    if (device) {
-        device.value << [name: body?.device?.roomName?.text(), model:body?.device?.modelName?.text(), serialNumber:body?.device?.serialNum?.text(), verified: true]
-    }
+    def dev = addChildDevice("synfinatic", "HTD GW-SL1", dni, theHub.id, 
+        [label: "${app.label}", name: "HTD GW-SL1")
+    log.trace "created ${dev.displayName} with id $dni"
 }
 
 private Integer convertHexToInt(hex) {
@@ -181,13 +68,13 @@ private String convertHexToIP(hex) {
 
 private String convertIPtoHex(ipAddress) { 
     String hex = ipAddress.tokenize( '.' ).collect {  String.format( '%02x', it.toInteger() ) }.join()
-    log.debug "IP address entered is $ipAddress and the converted hex code is $hex"
+    log.debug "IP address $ipAddress is converted to $hex"
     return hex
 
 }
 
 private String convertPortToHex(port) {
-    String hexport = port.toString().format( '%04x', port.toInteger() )
-    log.debug hexport
+    String hexport = port.toString().format('%04x', port.toInteger())
+    log.debug "Port $port is converted to $hexport"
     return hexport
 }
